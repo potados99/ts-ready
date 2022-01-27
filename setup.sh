af639200f14ab4f19032323d00303baa0cc2ee5b
potados99 <potados99@gmail.com>
2022-01-27 23:03:51 +0900

    refactor: sed in place 대치

--- setup.sh
+++ setup.sh
@@ -22,21 +22,11 @@ read repository
 # package.json
 ################################################################
 
-new_package_file_content=$(cat $package_file |
-sed "s|__NAME__|$name|g" |
-sed "s|__DESCRIPTION__|$description|g" |
-sed "s|__REPOSITORY__|$repository|g")
-
-echo "package.json 미리보기:"
-echo $new_package_file_content
-
-echo "이대로 만들까요?"
-read
-
-echo $new_package_file_content > $package_file
+sed -i "" "s|__NAME__|$name|g" $package_file
+sed -i "" "s|__DESCRIPTION__|$description|g" $package_file
+sed -i "" "s|__REPOSITORY__|$repository|g" $package_file
 
 echo "package.json 설정 완료."
-echo ""
 
 ################################################################
 # README.md
@@ -44,21 +34,15 @@ echo ""
 
 readme_file_content="# $name\n\n$description"
 
-echo "README.md 미리보기:"
-echo $readme_file_content
-
-echo "이대로 만들까요?"
-read
-
 echo $readme_file_content > $readme_file
 
 echo "README.md 설정 완료."
-echo ""
 
 ################################################################
 # Finishing
 ################################################################
 
+echo ""
 echo "이 스크립트를 삭제합니다."
 read
 
